0230b2f2a1346d9b348f98179edd84b70d732569
mbwright <mbwright@butler.edu>
2021-04-12 23:48:55 -0400

    done?

--- pmergeMyles_Wright.cpp
+++ pmergeMyles_Wright.cpp
@@ -1,7 +1,16 @@
-/*  Myles Wright
+/*  Myles Wright, Cathal Osullivan
     Butler University 2021
     CS452 PMERGE
 */
+
+
+//GUIDES THAT WE USED:
+//https://github.com/RachelBurke/CS452/blob/master/Project3/cloneWars.cpp
+//https://cse.buffalo.edu/faculty/miller/Courses/CSE702/Swati.Nair-Fall-2018.pdf
+//https://www.youtube.com/watch?v=_XOZ2IiP2nw
+//http://selkie.macalester.edu/csinparallel/modules/ParallelSorting/build/html/MergeSort/MergeSort.html
+//https://stanford.edu/~rezab/classes/cme323/S16/notes/Lecture04/cme323_lec4.pdf
+
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
@@ -13,155 +22,12 @@ using namespace std;
 // New compile and run commands for MPI!
 // mpicxx -o blah file.cpp
 // mpirun -q -np 32 blah
-
-int srank(int * arr, int x, int valtofind){ //MYLES: a fun binary search style algorithm that returns the value of where the value should be
-    cout << "inside of srank" << endl;
-    if (x==1){
-        if(valtofind<arr[0]){
-            return 0;
-        }
-        else{
-            return 1;
-        }
-    }
-    else{
-        if(valtofind < arr[x/2]){
-            return srank(arr, x/2, valtofind);
-        }
-        else{
-            return x/2 + srank(&arr[x/2], x/2, valtofind);
-        }
-    }
-
-}
-
-void insertionSort(int* array, int size){//CATHAL: hehe
-    cout << "inside insertion" << endl;
-    int k;
-    int j;
-    for (int i = 1; i < size; i++)
-    {
-        k = array[i];
-        j = i;
-        while (j > 0 && array[j - 1] > k)
-        {
-            array[j] = array[j - 1];
-            j--;
-        }
-        array[j] = k;
-    }
-}
-
-void smerge(int* A, int a_start, int a_end, int* B, int b_start, int b_end, int* C, int c_start, int c_end)
-{
-    cout << "Inside of smerge" << endl;
-	for(int i = c_start; i < c_end; i++)
-	{
-		//If there are elements left in A and either there are no elements left in B, 
-		//or the next A element is less than the next B element, place the next A element
-		//In the next C spot
-		if(a_start < a_end && (b_start == b_end || A[a_start] < B[b_start])){
-			C[i] = A[a_start++];
-		}
-		else{		//Else place the next B element in the next C spot
-			C[i] = B[b_start++];
-		}
-	}
-}
-
-void printArray(int * a, int size){
-    for(int i = 0; i < size; i++){
-        cout << a[i] << " ";
-    }
-    cout << "\n";
-}
-
-void clear(int*a, int n){
-    for(int i = 0; i<n; i++){
-        a[i] = 0;
-    }
-}
-
-
-void pmerge(int* A, int* B, int* C, int n, int my_rank, int p)		//Merge Array A and B, both size n/2, into array C, size n
-{
-    cout << "inside of pmerge" << endl;
-	int logn = log2(n/2);
-	int x = ceil((n/2)/logn);		//Number of sampled elements
-	
-	//Arrays to hold positions and ranks used to determine end points of sub arrays to merge using sequential merge
-	//lower case is used for local arrays to be filled out by individual processes and then all reduced into capital arrays
-	//i.e Allreduce(ar, AR, n, ...)
-	int* ar = new int[2*x + 2];
-	int* br = new int[2*x + 2];
-	int* Ar = new int[2*x + 2];
-	int* Br = new int[2*x + 2];
-	clear(ar,2*x+2);
-	clear(br,2*x+2);
-	clear(Ar,2*x+2);
-	clear(Br,2*x+2);
-	cout << "Test"<< endl;
-	for(int i = my_rank; i < x; i+=p){		//Fill in positions of sampled elements(in parallel)
-		ar[i] = 1 + i*logn;
-		br[i] = 1 + i*logn;
-	}
-    cout << "test 2" << endl;		
-	for(int i = my_rank; i < x; i+=p){
-        cout << "going to srank ONE" << endl;		//Fill in ranks of sampled elements(in parallel)
-		br[i+x] = srank(B, n/2, A[0+i*logn]);
-        cout << "going to srank TWO" << endl;
-		ar[i+x] = srank(A, n/2, B[0+i*logn]);
-        cout << "done with srank" << endl;
-	}
-    cout << "Test 3" << endl;
-	MPI_Allreduce(ar, Ar, 2*x+2, MPI_INT, MPI_SUM, MPI_COMM_WORLD);		//combine local arrays and broadcast
-    cout << "Allreduce 1 done" << endl;
-	MPI_Allreduce(br, Br, 2*x+2, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-	cout << "Allreduce done" << endl;
-	Ar[2*x] = 0;				//Fill in First Position(i = 0)
-	Ar[2*x + 1] = n/2;			//Fill in Last Position(i = n)
-	Br[2*x] = 0;				//Fill in First Position(i = 0)
-	Br[2*x + 1] = n/2;			//Fill in Last Position(i = n)
-    cout << "rank arrays before sort: " << endl;
-    printArray(Ar, 2*x);
-    printArray(Br, 2*x);
-	insertionSort(Ar, 2*x+2);			//Sort these to make sure they are in order
-	insertionSort(Br, 2*x+2);			//Sorted using insertion sort
-	cout << "rank arrays after sort: " << endl;
-    printArray(Ar, 2*x);
-    printArray(Br, 2*x);
-	int* localC = new int[n];		//Local C array for the sequnetial merges performed by individual process by striping.
-	clear(localC, n);
-	
-	//Striping the sequential merges
-	for(int i = my_rank; i < 2*x+1; i+=p){
-		smerge(A, Ar[i], Ar[i+1], B, Br[i], Br[i+1], localC, Ar[i] + Br[i], Ar[i+1]+Br[i+1]);
-	}
-	
-	//Combine each process merges into the resulting C array
-	MPI_Allreduce(localC, C, n, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-    cout << "array C:" << endl;
-    printArray(C, n);
-	
-}
-
-void merge_sort(int*a, int*b, int n, int my_rank, int p){
-    //MYLES: We have made a base for arrays less than 4 just use insertion because its better, could use another sort but I saw this one in a sort visualizer video on youtube and thought it looked cool so why not?
-    cout << "Inside of merge_sort" << endl;
-    if(n==4){
-        for(int i = 0; i < n; i++){
-            b[i] = a[i];
-        }
-        insertionSort(b, n);
-    }
-    else{
-        int*c = new int[n]; //basically we are creating a temp array to store the two halfs then run mergesort recursively
-        clear(c,n);
-        merge_sort(&a[0], &c[0], n/2, my_rank, p);
-        merge_sort(&a[n/2], &c[n/2], n - n/2, my_rank, p);
-        pmerge(&c[0], &c[n/2], &b[0], n, my_rank, p);
-    }
-}
+int srank(int * arr, int x, int valtofind);
+void insertionSort(int* array, int size);
+void smerge(int* A, int a_start, int a_end, int* B, int b_start, int b_end, int* C, int c_start, int c_end);
+void printArray(int * a, int size);
+void pmerge(int* A, int* B, int* C, int n, int my_rank, int p);
+void mergesort(int*a, int*b, int n, int my_rank, int p);
 
 int main(int argc, char * argv[]) {
 
@@ -212,6 +78,7 @@ int main(int argc, char * argv[]) {
     MPI_Bcast(a, (size/2), MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(b, size/2, MPI_INT, 0, MPI_COMM_WORLD);
     
+    cout << "process " << my_rank << "a:" << endl;
     cout << "boots withthe fur" << endl;
     merge_sort(a, b, size, my_rank, p);
     cout << "ASS" << endl;
@@ -226,4 +93,166 @@ int main(int argc, char * argv[]) {
 	MPI_Finalize();
 
 	return 0;
+}
+
+
+
+void insertionSort(int* array, int size){//CATHAL: hehe
+    cout << "inside insertion" << endl;
+    int k;
+    int j;
+    for (int i = 1; i < size; i++)
+    {
+        k = array[i];
+        j = i;
+        while (j > 0 && array[j - 1] > k)
+        {
+            array[j] = array[j - 1];
+            j--;
+        }
+        array[j] = k;
+    }
+}
+
+int srank(int * arr, int x, int valtofind){ //MYLES: a fun binary search style algorithm that returns the value of where the value should be
+    cout << "inside of srank" << endl;
+    if (x==1){
+        if(valtofind<arr[0]){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
+    else{
+        if(valtofind < arr[x/2]){
+            return srank(arr, x/2, valtofind);
+        }
+        else{
+            return x/2 + srank(&arr[x/2], x/2, valtofind);
+        }
+    }
+
+}
+
+void smerge(int* a, int a_start, int a_end, int* b, int b_start, int b_end, int* c, int c_start, int c_end)
+{
+    cout << "Inside of smerge" << endl;
+	for(int i = c_start; i < c_end; i++)
+	{
+		if(a_start < a_end && (b_start == b_end || a[a_start] < b[b_start])){
+			c[i] = a[a_start++];
+		}
+		else{		
+			c[i] = b[b_start++];
+		}
+	}
+}
+
+void printArray(int * a, int size){
+    for(int i = 0; i < size; i++){
+        cout << a[i] << " ";
+    }
+    cout << "\n";
+}
+
+void clear(int*arr, int x){ //fills with
+    for(int i = 0; i<x; i++){
+        arr[i] = 0;
+    }
+}
+
+
+void pmerge(int* a, int* b, int* c, int x, int my_rank, int p)		//Merge Array A and B, both size n/2, into array C, size n
+{
+    cout << "inside of pmerge" << endl;
+	int logn = log2(x/2);
+	int y = ceil((x/2)/logn);		//Number of sampled elements
+	
+	
+    int rsize = 2*y+2;
+	int* rankA = new int[rsize];
+	int* rankB = new int[rsize];
+	int* Ar = new int[rsize];
+	int* Br = new int[rsize];
+	clear(rankA,rsize);
+	clear(rankB,rsize);
+	clear(Ar,rsize);
+	clear(Br,rsize);
+	
+    
+
+	for(int i = my_rank; i < x; i+=p){		
+		rankA[i] = 1 + i*logn;
+		rankB[i] = 1 + i*logn;
+	}
+    cout << "test 2" << endl;		
+	for(int i = my_rank; i < y; i+=p){
+        cout << "going to srank ONE" << endl;		
+		rankA[i+x] = srank(a, x/2, A[0+i*logn]);
+        cout << "going to srank TWO" << endl;
+		rankB[i+x] = srank(b, x/2, B[0+i*logn]);
+        cout << "done with srank" << endl;
+	}
+
+    cout << "rankA: " << endl;
+    printArray(rankA, rsize);
+    cout << "rankB: " << endl;
+    printArray(rankB, rsize);
+    cout << "Ar: " << endl;
+    printArray(Ar, rsize);
+    cout << "Br: " << endl;
+    printArray(Br, rsize);
+
+    cout << "Test 3" << endl;
+	MPI_Allreduce(rankA, Ar, rsize, MPI_INT, MPI_SUM, MPI_COMM_WORLD);		
+    cout << "Allreduce 1 done" << endl;
+	MPI_Allreduce(rankB, Br, rsize, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+	cout << "Allreduce done" << endl;
+
+	Ar[2*y] = 0;				
+	Ar[2*y + 1] = x/2;			
+	Br[2*y] = 0;				
+	Br[2*y + 1] = x/2;
+
+
+    cout << "rank arrays before sort: " << endl;
+    printArray(Ar, 2*y);
+    printArray(Br, 2*y);
+	insertionSort(Ar, rsize);			
+	insertionSort(Br, rsize);			
+	cout << "rank arrays after sort: " << endl;
+    printArray(Ar, 2*y);
+    printArray(Br, 2*y);
+	int* localC = new int[x];		
+	clear(localC, y);
+	
+
+	for(int i = my_rank; i < 2*x+1; i+=p){
+		smerge(a, Ar[i], Ar[i+1], b, Br[i], Br[i+1], localC, Ar[i] + Br[i], Ar[i+1]+Br[i+1]);
+	}
+	
+	//Combine each process merges into the resulting C array
+	MPI_Allreduce(localC, c, x, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    cout << "array C:" << endl;
+    printArray(c, x);
+	
+}
+
+void mergesort(int*a, int*b, int n, int my_rank, int p){
+    //MYLES: We have made a base for arrays less than 4 just use insertion because its better, could use another sort but I saw this one in a sort visualizer video on youtube and thought it looked cool so why not?
+    cout << "Inside of merge_sort" << endl;
+    if(n==4){
+        for(int i = 0; i < n; i++){
+            b[i] = a[i];
+        }
+        insertionSort(b, n);
+    }
+    else{
+        int*c = new int[n]; //basically we are creating a temp array to store the two halfs then run mergesort recursively
+        clear(c,n);
+        mergesort(&a[0], &c[0], n/2, my_rank, p);
+        mergesort(&a[n/2], &c[n/2], n - n/2, my_rank, p);
+        pmerge(&c[0], &c[n/2], &b[0], n, my_rank, p);
+    }
 }
